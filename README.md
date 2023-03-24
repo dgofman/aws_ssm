5a8d1260a822cc915bf8705e84c973fdfd8b5c88
David Gofman <dgofman@gmail.com>
2023-03-23 20:18:37 -0700

    version 1.0.1

--- README.md
+++ README.md
@@ -1,4 +1,103 @@
-## Examples of the complete Signature Version 4 signing process
+# AWS Systems Manager Parameter Store
 
-This section shows example programs written in Python that illustrate how to work with Signature Version 4 in Amazon. 
-We deliberately wrote these example programs to be simple to make it easier to understand the overall process of signing Amazon requests.
+AWS Systems Manager Parameter Store provides secure, hierarchical storage for configuration data management and secrets management. It can store data such as passwords, database strings, Amazon Machine Image (AMI) IDs and license codes as parameter values.
+
+
+<p align="center">
+<a href="https://raw.githubusercontent.com/dgofman/aws_ssm/master/LICENSE"><img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="License: MIT"></a>
+<a href="https://pub.dev/packages/aws_ssm/install"><img src="https://img.shields.io/badge/pub-v1.0.0-blue" alt="aws_ssm"></a>
+</p>
+
+
+## Prerequisites
+##### User Pool
+<a href="https://docs.aws.amazon.com/cognito/latest/developerguide/tutorial-create-user-pool.html">Tutorial: Creating a user pool</a>
+
+- Store "User Pool ID" value (User pool overview)
+- There must be at least one client application in the "Application Integration" tab "Application Client List -> Create Application Client".
+- Store the value "Client ID" (App client information)
+
+##### Create Federated identities
+<a href="https://docs.aws.amazon.com/cognito/latest/developerguide/tutorial-create-identity-pool.html">Tutorial: Creating an identity pool</a>
+
+- Assign "User pool ID" and "Client ID" to the fields in "Authentication providers -> Cognito"
+- Create a custom SSM Role: "Identity pool -> Edit identity pool -> Authenticated role"
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameters"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+### Get cognito user JWT id token
+
+```dart
+import 'package:amazon_cognito_identity_dart_2/cognito.dart';
+...
+final user = CognitoUser({{COGNITO_USER_NAME}}, CognitoUserPool(
+  userPoolId,
+  clientId,
+));
+CognitoUserSession? session = await user.authenticateUser(AuthenticationDetails(
+  username: {{COGNITO_USER_NAME}},
+  password: {{COGNITO_USER_PASSWORD}},
+));
+final idToken = session!.getIdToken().getJwtToken();
+```
+
+### Get list of AWS Systems Manager Parameters
+
+```dart
+import 'aws_ssm.dart';
+...
+try {
+  final names = ['db-url', 'my-username', 'my-password'];
+  final ssm = AwsSSM(region, userPoolId, identityPoolId);
+  final values = await ssm.getListParams(idToken, names);
+  print(values); //['db-url-value', 'my-username-value', 'my-password-value']
+} catch (ex) {
+  print(ex);
+}
+```
+
+### Get map of AWS Systems Manager Parameters
+
+```dart
+import 'aws_ssm.dart';
+...
+try {
+    final names = ['db-url', 'my-username', 'my-password'];
+    final ssm = AwsSSM(region, userPoolId, identityPoolId);
+    final values = await ssm.getListParams(idToken, names);
+    print(values); //{'db-url': 'db-url-value', 'my-username': 'my-username-value', 'my-password': 'my-password-value'}
+} catch (ex) {
+  print(ex);
+}
+```
+
+## List of API's
+
+```dart
+import 'aws_ssm.dart';
+...
+try {
+    final ssm = AwsSSM(region, userPoolId, identityPoolId);
+    final credentials = await ssm.getCognitoCredentialsForIdentity(idToken);
+    final payload = ssm.createPayload(names, true);
+    final datetime = await ssm.getServerDateTime();
+    final headers = ssm.createAWS4Header(credentials, payload, datetime);
+    final params = await ssm.getParameters(headers, payload);
+    final values = ssm.toMap<String>(names, params);
+    print(values);
+} catch (ex) {
+  print(ex);
+}
+```
--- lib/aws_ssm.dart
+++ lib/aws_ssm.dart
@@ -13,87 +13,119 @@
  * limitations under the License.
  */
 
-import 'package:amazon_cognito_identity_dart_2/cognito.dart';
-import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-import 'package:flutter/foundation.dart';
+import 'package:convert/convert.dart' show hex;
+import 'package:crypto/crypto.dart' show sha256, Hmac;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:http/http.dart' as http;
-import 'dart:convert' show json;
+import 'dart:convert' show json, utf8;
 
 //AWS SSM Version 4
 // https://docs.amazonaws.cn/en_us/general/latest/gr/sigv4-signed-request-examples.html
-// Error: pub get failed (1; So, because nats_client depends on both flutter_localizations any from sdk and aws_ssm_api any, version solving failed.)
+class ResponseException {
+  final String message;
+  final dynamic data;
+  final String errorType;
+  final int statusCode;
+  ResponseException(this.message, this.data, this.errorType, this.statusCode);
+}
 
 class AwsSSM {
-
   final String region;
-  final CognitoCredentials credentials;
-  final AwsSigV4Client awsSigV4Client;
+  final String userPoolId;
+  final String identityPoolId;
 
-  AwsSSM(this.region, this.credentials) :
-        awsSigV4Client = AwsSigV4Client(
-            credentials.accessKeyId!, credentials.secretAccessKey!,
-            'https://ssm.$region.amazonaws.com',
-            sessionToken: credentials.sessionToken,
-            serviceName: 'ssm',
-            region: region);
+  AwsSSM(this.region, this.userPoolId, this.identityPoolId);
 
-  SigV4Request _createRequest(String target, String? datetime, dynamic body) {
-    return SigV4Request(awsSigV4Client,
-        datetime: datetime,
-        method: 'POST', path: '/',
-        headers: Map<String, String>.from(
-            {'Content-Type': 'application/x-amz-json-1.1; charset=utf-8',
-              'X-Amz-Target': target}),
-        body: body);
+  Future<dynamic> getParams(String? idToken, List<String> names) async {
+    final credentials = await getCognitoCredentialsForIdentity(idToken);
+    final payload = createPayload(names);
+    final datetime = await getServerDateTime();
+    final headers = createAWS4Header(credentials, payload, datetime);
+    return getParameters(headers, payload);
   }
 
-  Future<ValueSSM> _send(SigV4Request request) async {
-    http.Response? res;
-    Map<String, String> headers = {};
-    request.headers!.forEach((key, value) {
-      if (value != null) {
-        headers[key] = value;
-      }
-    });
-    try {
-      res = await http.post(Uri.parse(request.url!), headers: headers, body: request.body);
-    } catch (e) {
-      throw Exception('SSMError[$res.statusCode] $res');
-    }
-    if (res.statusCode < 200 || res.statusCode >= 300 || res.body.isEmpty) {
-      throw Exception('SSMError[$res.statusCode] ${res.body}');
-    }
-    return ValueSSM(json.decode(res.body));
+  Future<List<T?>> getListParams<T>(String? idToken, List<String> names) async {
+    final params = await getParams(idToken, names);
+    return toList<T>(names, params);
   }
 
-  Future<String?> getDateTime() async {
-    if (!kIsWeb) {
-      try {
-        http.Response res = await http.get(Uri.parse('https://mws.amazonservices.com/'));
-        RegExp exp = RegExp(r'timestamp="(.*)"', multiLine: true);
-        final match = exp.firstMatch(res.body);
-        if (match != null) {
-          return match.group(1).toString()
-              .replaceAll(RegExp(r'\.\d*Z$'), 'Z')
-              .replaceAll(RegExp(r'[:-]|\.\d{3}'), '');
-        }
-        // ignore: empty_catches
-      } catch (e) {}
-    }
-    return Future.value(null);
+  Future<Map<String, T?>> getMapParams<T>(String? idToken, List<String> names) async {
+    final params = await getParams(idToken, names);
+    return toMap<T>(names, params);
   }
 
-  Future<ValueSSM> getParameters(List<String> names, {
-    bool withDecryption = false,
-    String? datetime}) {
-    final req = _createRequest('AmazonSSM.GetParameters', datetime, {
-      'Names': names, 'WithDecryption': withDecryption
-    });
-    return _send(req);
+  //_cognitoUserSession!.getIdToken().getJwtToken()
+  Future<Map<String, dynamic>> getCognitoCredentialsForIdentity(String? idToken) async {
+    final uri = 'https://cognito-identity.$region.amazonaws.com/';
+    final headers = {'Content-Type': 'application/x-amz-json-1.1', 'X-Amz-Target': 'AWSCognitoIdentityService.GetId'};
+    final body = {
+      'IdentityPoolId': identityPoolId,
+      'Logins': {'cognito-idp.$region.amazonaws.com/$userPoolId': idToken}
+    };
+    dynamic data = await _request(uri, headers, body);
+    body['IdentityId'] = data?['IdentityId'];
+    headers['X-Amz-Target'] = 'AWSCognitoIdentityService.GetCredentialsForIdentity';
+    return await _request(uri, headers, body);
   }
 
-  List<T?> getValues<T>(List<String> names, ValueSSM value) {
-    ValueSSM parameters = value.parameters;
+  Map<String, dynamic> createPayload(dynamic names, [bool withDecryption = true]) {
+    return {'Names': names, 'WithDecryption': withDecryption};
+  }
+
+  Map<String, String> createAWS4Header(dynamic credentials, Map<String, dynamic> payload, [String? datetime]) {
+    final accessKeyId = credentials['Credentials']['AccessKeyId'];
+    final secretKey = credentials['Credentials']['SecretKey'];
+    final sessionToken = credentials['Credentials']['SessionToken'];
+
+    datetime ??= getDateTime();
+
+    Map<String, String> headers = {
+      'accept': 'application/json',
+      'content-type': 'application/x-amz-json-1.1; charset=utf-8',
+      'host': 'ssm.$region.amazonaws.com',
+      'x-amz-date': datetime,
+      'x-amz-target': 'AmazonSSM.GetParameters'
+    };
+
+    final canonicalRequest = [
+      'POST',
+      '/',
+      '',
+      headers.map((key, value) => MapEntry(key, '$key:$value')).values.join('\n') + '\n',
+      headers.keys.join(';'),
+      hex.encode(sha256.convert(utf8.encode(json.encode(payload))).bytes)
+    ].join('\n');
+
+    final hashedCanonicalRequest = hex.encode(sha256.convert(utf8.encode(canonicalRequest)).bytes);
+    final credentialScope = '${datetime.substring(0, 8)}/$region/ssm/aws4_request';
+    final stringToSign = ['AWS4-HMAC-SHA256', datetime, credentialScope, hashedCanonicalRequest].join('\n');
+
+    final signingKey = _sign(
+        _sign(_sign(_sign(utf8.encode('AWS4$secretKey'), datetime.substring(0, 8)), region), 'ssm'), 'aws4_request');
+    final signature = hex.encode(_sign(signingKey, stringToSign));
+
+    headers['Authorization'] = [
+      'AWS4-HMAC-SHA256',
+      ' Credential=',
+      accessKeyId,
+      '/',
+      credentialScope,
+      ', SignedHeaders=',
+      headers.keys.join(';'),
+      ', Signature=',
+      signature
+    ].join();
+    headers['x-amz-security-token'] = sessionToken;
+
+    return headers;
+  }
+
+  Future<dynamic> getParameters(Map<String, String> headers, Map<String, dynamic> payload) async {
+    return _request('https://ssm.$region.amazonaws.com/', headers, payload);
+  }
+
+  List<T?> toList<T>(List<String> names, dynamic data) {
+    ValueSSM parameters = ValueSSM(data).parameters;
     List<T?> values = List.filled(parameters.length, null);
     for (int i = 0; i < parameters.length; i++) {
       ValueSSM value = parameters.get(i);
@@ -104,6 +136,75 @@ class AwsSSM {
     }
     return values;
   }
+
+  Map<String, T?> toMap<T>(List<String> names, dynamic data) {
+    ValueSSM parameters = ValueSSM(data).parameters;
+    Map<String, T?> values = {};
+    for (int i = 0; i < parameters.length; i++) {
+      ValueSSM value = parameters.get(i);
+      final index = names.indexOf(value.name);
+      if (index != -1) {
+        values[value.name] = value.value;
+      }
+    }
+    return values;
+  }
+
+  List<int> _sign(List<int> key, String message) {
+    final hmac = Hmac(sha256, key);
+    final dig = hmac.convert(utf8.encode(message));
+    return dig.bytes;
+  }
+
+  Future<dynamic> _request(String uri, Map<String, String>? headers, Object? body) async {
+    dynamic data;
+    try {
+      final response = await http.post(Uri.parse(uri), headers: headers, body: json.encode(body));
+      try {
+        data = json.decode(utf8.decode(response.bodyBytes));
+      } catch (_) {
+        // expect json
+      }
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        String errorType = 'UnknownError';
+        for (final header in response.headers.keys) {
+          if (header.toLowerCase() == 'x-amzn-errortype') {
+            errorType = response.headers[header]!.split(':')[0];
+            break;
+          }
+        }
+        throw ResponseException('ResponseException', data, errorType, response.statusCode);
+      }
+    } catch (ex) {
+      rethrow;
+    }
+    return data;
+  }
+
+  String getDateTime() {
+    return DateTime.now()
+        .toUtc()
+        .toString()
+        .replaceAll(RegExp(r'\.\d*Z$'), 'Z')
+        .replaceAll(RegExp(r'[:-]|\.\d{3}'), '')
+        .split(' ')
+        .join('T');
+  }
+
+  Future<String> getServerDateTime() async {
+    if (!kIsWeb) {
+      try {
+        http.Response res = await http.get(Uri.parse('https://mws.amazonservices.com/'));
+        RegExp exp = RegExp(r"timestamp='(.*)'", multiLine: true);
+        final match = exp.firstMatch(res.body);
+        if (match != null) {
+          return match.group(1).toString().replaceAll(RegExp(r'\.\d*Z$'), 'Z').replaceAll(RegExp(r'[:-]|\.\d{3}'), '');
+        }
+        // ignore: empty_catches
+      } catch (e) {}
+    }
+    return getDateTime();
+  }
 }
 
 class ValueSSM {
@@ -112,21 +213,27 @@ class ValueSSM {
   ValueSSM get invalidParameters {
     return ValueSSM(_value['InvalidParameters']);
   }
+
   ValueSSM get parameters {
     return ValueSSM(_value['Parameters']);
   }
+
   ValueSSM get(int index) {
     return ValueSSM(_value.length > index ? _value[index] : null);
   }
+
   dynamic prop(String key) {
     return _value[key];
   }
+
   dynamic get value {
     return prop('Value');
   }
+
   dynamic get name {
     return prop('Name');
   }
+
   get length {
     return _value.length;
   }
